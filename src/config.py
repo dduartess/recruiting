1e397f682a9575c70451b73c836a37d6aeda0413
dduartess <dandandsoares@gmail.com>
2025-12-18 22:50:44 -0300

    📦 FEAT: create configurations PostgreSQL

--- src/config.py
+++ src/config.py
@@ -5,5 +5,13 @@ load_dotenv()
 
 GENERAL = {
     "SECRET_KEY": os.getenv("SECRET_KEY"),
-    "DEBUG": os.getenv("DEBUG"), 
+    "DEBUG": os.getenv("DEBUG"),
+}
+
+DB_CONFIG = {
+    "NAME": os.getenv("DB_NAME"),
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST"),
+    "PORT": os.getenv("DB_PORT"),
 }
--- src/core/settings.py
+++ src/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from config import GENERAL
+from config import GENERAL, DB_CONFIG
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_CONFIG['NAME'],
+        'USER': DB_CONFIG['USER'],
+        'PASSWORD': DB_CONFIG['PASSWORD'],
+        'HOST': DB_CONFIG['HOST'],
+        'PORT': DB_CONFIG['PORT'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
